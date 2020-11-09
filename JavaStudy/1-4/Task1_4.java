8cedaafb58113ad35dd48c4ac31acb56022da709
TomohiroOzawa <72886384+TomohiroOzawa@users.noreply.github.com>
2020-11-09 19:52:18 +0900

    制御文課題提出
    制御文課題提出

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -25,17 +25,17 @@ public class Task1_4 {
         String pass = "pass";
         // ② 名前が「 alice 」、パスワードが「 pass 」であった場合、「 ログイン成功です 」と出力してください。
         if(name.equals(name1) && pass.equals(pass1)) {
-            System.out.println("ログイン成功です"); 
+            System.out.println(CONST_MSG_SUCCESS); 
         // ③ 名前のみ正しい場合、「 パスワードに誤りがあります。 」と出力してください。
-        } else if(!pass.equals(pass1) && name.equals(name1)) {
-            System.out.println("パスワードに誤りがあります。");
+        } else if(name.equals(name1)) {
+            System.out.println(CONST_MSG_ERROR_PASS);
         // ④ パスワードのみ正しい場合、「 名前に誤りがあります。 」と出力してください。
-        } else if(!name.equals(name1) && pass.equals(pass1)) {
-            System.out.println("パスワードに誤りがあります。");
+        } else if(pass.equals(pass1)) {
+            System.out.println(CONST_MSG_ERROR_NAME);
 
         // ⑤ 名前もパスワードも間違っていた場合は、「 入力情報に誤りがあります。 」と出力してください。
         } else {
-            System.out.println("入力情報に誤りがあります。");
+            System.out.println(CONST_MSG_ERROR_INPUT);
     }
 
  }
